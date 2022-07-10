77bddd7d5fe5a6bf91da520402909e22e387b438
Artem Denisov <art.dnsv@gmail.com>
2022-07-11 02:30:19 +0300

    Changed models

--- src/items/admin.py
+++ src/items/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Name)
-admin.site.register(models.Price)
-admin.site.register(models.YearOfPublication)
-admin.site.register(models.Format)
-admin.site.register(models.Page)
-admin.site.register(models.ISBN)
-admin.site.register(models.Weight)
-admin.site.register(models.InStock)
-admin.site.register(models.ReadingAge)
+admin.site.register(models.Book)
+
--- src/items/models.py
+++ src/items/models.py
@@ -4,69 +4,62 @@ from isbn_field import ISBNField
 
 # Create your models here.
 
-class Name(models.Model):
+class Book(models.Model):
     name = models.CharField(
         verbose_name='Name of the item',
         max_length=25
     )
+    def __str__(self):
+        return str(self.name)
     
-class Price(models.Model):
     price = models.DecimalField(
-        verbose_name='Price',
+        verbose_name='Price(BYN)',
         max_digits=10, 
         decimal_places=2
         )
-    def __str__(self):
-        return str(self.price) + " BYN"
     
-class YearOfPublication(models.Model):
     year = models.PositiveSmallIntegerField(
-        verbose_name="Release"
+        verbose_name="Release year"
     )
-
-class Page(models.Model):
-    pages = models.PositiveSmallIntegerField(
-        verbose_name="Print lenth"
-    )
-    def __str__(self):
-        return str(self.pages) + " pages"
     
-
-class Format(models.Model):
+    pages = models.PositiveSmallIntegerField(
+        verbose_name="Print lenght"
+    )
+    
     CHOICES = (
         ('Hard',"Hardcover"),
-        ('Soft',"Paperback")
+        ('Soft',"Paperback"),
     )
     cover = models.CharField(
+        verbose_name="Book type",
         max_length=30,
-        verbose_name="Format",
         choices=CHOICES 
     )
-    
-class ISBN(models.Model):
+    dimensions = models.CharField(
+        verbose_name="Dimensions",
+        max_length=25
+    )
     isbn = ISBNField()
     
-class Weight(models.Model):
     weight = models.PositiveSmallIntegerField(
-        verbose_name='Item weight'
+        verbose_name='Item weight(gram)'
     )
-    def __str__(self):
-        return str(self.weigt) + " gram"
-
-class InStock(models.Model):
-    CHOICES = (
-        ('Y',"Yes"),
-        ('N',"No")
+    
+    OPTIONS = (
+        ('y',"Yes"),
+        ('n',"No")
     )
-    cover = models.CharField(
+    instock = models.CharField(
         max_length=30,
         verbose_name="In stock",
-        choices=CHOICES 
+        choices=OPTIONS 
     )
-class ReadingAge(models.Model):
+    
     age = models.CharField(
         verbose_name="Reading Age",
         max_length=3
     )
-    
-    
+
+    availible = models.PositiveIntegerField(
+        verbose_name="Books available"
+    )
